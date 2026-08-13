0d2f878703e794b748e8be8e464260865433c5d9
Wart Claes <wart.claes@gmail.com>
2026-08-13 16:33:14 +0200

    Add API reference section to the demo/docs page

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CmdkPaletteComponent } from 'ngx-cmdk';
+import { ApiReference } from './api-reference';
 import { DemoActivityLog } from './demo-activity-log';
 import { DemoActions } from './demo-actions';
 import { DemoNav } from './demo-nav';
 
 @Component({
   selector: 'app-root',
-  imports: [CmdkPaletteComponent, DemoActions, DemoNav],
+  imports: [CmdkPaletteComponent, DemoActions, DemoNav, ApiReference],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
