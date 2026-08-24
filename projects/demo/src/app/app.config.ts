e9844b4983a925117ecf2ca96ecd2989def36d2f
Wart Claes <wart.claes@gmail.com>
2026-08-24 09:09:07 +0200

    Wire Favourites and Settings into the demo app

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideCmdk } from 'ngx-cmdk';
 
+export const demoNavigateTarget = {
+  current: (path: string) => console.log('[ngx-cmdk demo] navigate:', path),
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideCmdk({ shortcut: 'mod+k', recentSearchesStorageKey: () => 'ngx-cmdk-demo-recents' }),
+    provideCmdk({
+      shortcut: 'mod+k',
+      recentSearchesStorageKey: () => 'ngx-cmdk-demo-recents',
+      favouritesStorageKey: () => 'ngx-cmdk-demo-favourites',
+      navigate: (path) => demoNavigateTarget.current(path),
+    }),
   ],
 };
